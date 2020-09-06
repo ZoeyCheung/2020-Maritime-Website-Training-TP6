588c57b3764d893f6d1ef67f8a233d676868fbf5
xiafuxiangfei <xiafuxiangfei@sina.com>
2020-09-06 19:52:54 +0800

    更新数据库

--- database/ecommerce.sql
+++ database/ecommerce.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50724
  File Encoding         : 65001
 
- Date: 27/07/2020 18:23:29
+ Date: 06/09/2020 19:52:28
 */
 
 SET NAMES utf8mb4;
@@ -23,24 +23,24 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `ec_goods`;
 CREATE TABLE `ec_goods` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `goods_sn` varchar(255) DEFAULT NULL,
-  `goods_name` varchar(255) DEFAULT NULL,
-  `thumb_img` varchar(255) DEFAULT NULL,
-  `goods_quantity` int(11) DEFAULT '1000',
-  `origin_price` decimal(10,2) DEFAULT NULL,
-  `goods_price` decimal(10,2) DEFAULT NULL,
-  `percentage` int(11) DEFAULT '0',
-  `goods_standard` varchar(255) DEFAULT NULL,
-  `goods_description` text,
-  `type_id` int(11) DEFAULT NULL,
-  `is_recommend` tinyint(1) DEFAULT '0',
-  `remarks` varchar(255) DEFAULT NULL,
-  `create_time` int(11) DEFAULT NULL,
-  `update_time` int(11) DEFAULT NULL,
-  `delete_time` int(11) DEFAULT NULL,
+  `goods_sn` varchar(255) DEFAULT NULL COMMENT '商品编号',
+  `goods_name` varchar(255) DEFAULT NULL COMMENT '商品名称',
+  `thumb_img` varchar(255) DEFAULT NULL COMMENT '缩略图',
+  `goods_quantity` int(11) DEFAULT '1000' COMMENT '数量',
+  `origin_price` decimal(10,2) DEFAULT NULL COMMENT '原价',
+  `goods_price` decimal(10,2) DEFAULT NULL COMMENT '商城价格',
+  `percentage` int(11) DEFAULT '0' COMMENT '优惠比例',
+  `goods_standard` varchar(255) DEFAULT NULL COMMENT '规格',
+  `goods_description` text COMMENT '描述',
+  `type_id` int(11) DEFAULT NULL COMMENT '类别ID，外键',
+  `is_recommend` tinyint(1) DEFAULT '0' COMMENT '是否在首页推荐，0不推荐，1推荐',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
+  `update_time` int(11) DEFAULT NULL COMMENT '更新时间',
+  `delete_time` int(11) DEFAULT NULL COMMENT '删除时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `goods_sn` (`goods_sn`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=166 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=165 DEFAULT CHARSET=utf8 COMMENT='商品表';
 
 -- ----------------------------
 -- Records of ec_goods
@@ -52,11 +52,7 @@ INSERT INTO `ec_goods` VALUES (155, 'sn0003', 'WOOD TABLET DECK', 'goods/2020072
 INSERT INTO `ec_goods` VALUES (156, 'sn0004', 'STONE CARPET', 'goods/20200727/7624466a68704871f622bba6913bc646.jpg', 1000, 34.00, 34.00, 0, '', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p>', NULL, 0, NULL, 1595819207, 1595839592, NULL);
 INSERT INTO `ec_goods` VALUES (157, 'sn0005', 'POLY KNIFE', 'goods/20200727/b030de1f4d6ec5d972c478e633627aa8.jpg', 1000, 8.00, 8.00, 0, '', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p>', NULL, 0, NULL, 1595819207, 1595843542, NULL);
 INSERT INTO `ec_goods` VALUES (158, 'sn0006', 'DUTCH GREY BAG', 'goods/20200727/e6a9f22e3445925128cdeb99e707ada1.jpg', 1000, 33.00, 30.00, 10, '', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p>', NULL, 1, NULL, 1595819207, 1595839552, NULL);
-INSERT INTO `ec_goods` VALUES (159, 'sn0007', 'MONACO SET LAMP', 'goods/20200727/8d56d50ca396124fdc855ab45fe348f8.jpg', 1000, 19.00, 19.00, 0, '', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p>', NULL, 0, NULL, 1595819207, 1595839532, NULL);
-INSERT INTO `ec_goods` VALUES (160, 'sn0008', 'COSMO ICE BOTTLE', 'goods/20200727/3407d2e8ef26792bc0b2fb60e7daf52e.jpg', 1000, 27.00, 27.00, 0, '个', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p>', NULL, 0, NULL, 1595819207, 1595839514, NULL);
-INSERT INTO `ec_goods` VALUES (161, 'sn0009', 'COGEN WARM LAMP', 'goods/20200727/1eb47130ec014172e03bed180ae54829.jpg', 1000, 56.00, 56.00, 0, '', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p>', NULL, 1, NULL, 1595819207, 1595839418, NULL);
-INSERT INTO `ec_goods` VALUES (162, 'sn0010', 'GREEN CARD PEN', 'goods/20200727/09f95baa12df3b80206c460508ab9914.jpg', 1000, 12.00, 12.00, 0, '12支/盒', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p>', NULL, 0, NULL, 1595819207, 1595839878, NULL);
-INSERT INTO `ec_goods` VALUES (163, 'sn0011', 'CONFETTI LAMP', 'goods/20200727/1840683b169ed2662ed2ec3c2c731a55.jpg', 1000, 60.00, 60.00, 0, '支', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p>', NULL, 0, NULL, 1595819207, 1595839861, NULL);
+INSERT INTO `ec_goods` VALUES (159, 'sn0007', 'MONACO SET LAMP', 'goods/20200727/8d56d50ca396124fdc855ab45fe348f8.jpg', 1000, 19.00, 19.00, 0, '', '<p>Dutch Bag crafts high quality tees and accessories that are comfortable,stylish, and imbued with the laid back spirit of Taffle. Comfortably holds your sneakers and your laptop in addition to a plethora of other necessities, while keeping everything organized and well protected.</p> <p><strong>Material:</strong> Body — 100% cotton trims / other — 100% polyurethane.</p><p><strong>Care:</strong> Wipe clean with a damp sponge, do not dry clean, do not iron</p><p><img src=\"/storage/uploads/20200906/0c5ed743a7ffc4ce308280aa8445f6fc.jpg\" alt=\"undefined\"><br></p>', NULL, 0, NULL, 1595819207, 1599389715, NULL);
 COMMIT;
 
 -- ----------------------------
@@ -65,13 +61,13 @@ COMMIT;
 DROP TABLE IF EXISTS `ec_goods_img`;
 CREATE TABLE `ec_goods_img` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `goods_id` int(11) DEFAULT NULL,
-  `img_url` varchar(255) DEFAULT NULL,
-  `create_time` int(11) DEFAULT NULL,
-  `update_time` int(11) DEFAULT NULL,
-  `delete_time` int(11) DEFAULT NULL,
+  `goods_id` int(11) DEFAULT NULL COMMENT '商品ID，外键',
+  `img_url` varchar(255) DEFAULT NULL COMMENT '图片地址',
+  `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
+  `update_time` int(11) DEFAULT NULL COMMENT '更新时间',
+  `delete_time` int(11) DEFAULT NULL COMMENT '删除时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8 COMMENT='商品预览图片表';
 
 -- ----------------------------
 -- Records of ec_goods_img
@@ -85,36 +81,22 @@ INSERT INTO `ec_goods_img` VALUES (21, NULL, NULL, NULL, NULL, NULL);
 COMMIT;
 
 -- ----------------------------
--- Table structure for ec_type
--- ----------------------------
-DROP TABLE IF EXISTS `ec_type`;
-CREATE TABLE `ec_type` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255) DEFAULT NULL,
-  `remarks` varchar(255) DEFAULT NULL,
-  `create_time` int(11) DEFAULT NULL,
-  `update_time` int(11) DEFAULT NULL,
-  `delete_time` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
 -- Table structure for ec_users
 -- ----------------------------
 DROP TABLE IF EXISTS `ec_users`;
 CREATE TABLE `ec_users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `password` varchar(255) CHARACTER SET utf8 NOT NULL DEFAULT '111111',
-  `nickname` varchar(255) CHARACTER SET utf8 DEFAULT '',
-  `is_admin` tinyint(1) DEFAULT '0',
-  `remarks` varchar(255) CHARACTER SET utf8 DEFAULT '',
-  `create_time` int(11) DEFAULT NULL,
-  `update_time` int(11) DEFAULT NULL,
-  `delete_time` int(11) DEFAULT NULL,
+  `username` varchar(255) NOT NULL COMMENT '用户名（手机号）',
+  `password` varchar(255) NOT NULL DEFAULT '111111' COMMENT '密码',
+  `nickname` varchar(255) DEFAULT '' COMMENT '昵称',
+  `is_admin` tinyint(1) DEFAULT '0' COMMENT '是否是管理员，1管理员，2普通用户',
+  `remarks` varchar(255) DEFAULT '' COMMENT '备注',
+  `create_time` int(11) DEFAULT NULL COMMENT '创建时间',
+  `update_time` int(11) DEFAULT NULL COMMENT '更新时间',
+  `delete_time` int(11) DEFAULT NULL COMMENT '删除时间',
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=68 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8 COMMENT='用户表\n';
 
 -- ----------------------------
 -- Records of ec_users
